886b16dac8762ba1ac2be695cdcf854097a2624c
algo-rhythmical <102335013+algo-rhythmical@users.noreply.github.com>
2026-02-11 15:30:24 -0600

    fix: applied AppLayout to App.tsx and removed from RouteConfig

--- Inksac-Web/src/routes/RouteConfig.tsx
+++ Inksac-Web/src/routes/RouteConfig.tsx
@@ -2,26 +2,11 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { HomePage } from "../pages/home/home-page";
 import { NotFoundPage } from "../pages/not-found";
 import { routes } from "./RouteIndex";
-import { AppLayout } from "../components/layout/app-layout";
 
 export const RouteConfig = () => (
   <Routes>
-    <Route
-      path={routes.home}
-      element={
-        <AppLayout>
-          <HomePage />
-        </AppLayout>
-      }
-    />
+    <Route path={routes.home} element={<HomePage />} />
     <Route path={routes.root} element={<Navigate to={routes.home} />} />
-    <Route
-      path="*"
-      element={
-        <AppLayout>
-          <NotFoundPage />
-        </AppLayout>
-      }
-    />
+    <Route path="*" element={<NotFoundPage />} />
   </Routes>
 );
