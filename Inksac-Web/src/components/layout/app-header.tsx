66caeb21aff473ddb9f0b7cecb026ae0c602ea4a
Anna <anna.valdez@selu.edu>
2026-04-06 21:31:03 -0500

    logo
    logo in the app bar and the browser tab

--- Inksac-Web/src/components/layout/app-header.tsx
+++ Inksac-Web/src/components/layout/app-header.tsx
@@ -1,5 +1,6 @@
 import { Group, Burger, Title } from "@mantine/core";
 import type { ReactNode } from "react";
+import logo from "../../assets/logo.svg";
 
 type AppHeaderProps = {
   opened: boolean;
@@ -17,7 +18,13 @@ export const AppHeader = ({
   return (
     <Group h="100%" px="md" wrap="nowrap">
       <Burger opened={opened} onClick={toggle} />
-      <Title order={3}>{title}</Title>
+
+      <Group gap="xs" wrap="nowrap" align="center">
+        <img src={logo} alt="logo" style={{ width: 90, height: 90 }} />
+        <Title order={3} size={40}>
+          {title}
+        </Title>
+      </Group>
 
       <Group gap="xs" wrap="nowrap" style={{ marginLeft: "auto" }}>
         {actions}
