1c621ef57c7d9fea43fd144dfa2e6dccd0afd86a
Caleb vv <calebviverito@gmail.com>
2026-05-01 22:26:49 -0500

    color/opacity inputs

--- Inksac-Web/src/components/layout/room-layout.tsx
+++ Inksac-Web/src/components/layout/room-layout.tsx
@@ -148,7 +148,7 @@ const RoomLayoutContext = createContext<RoomLayoutContextValue>({
   setActiveTool: () => {},
   shapeType: "line",
   setShapeType: () => {},
-  color: "#ffffffff",
+  color: "rgb(0, 0, 0)",
   toggleSidebar: () => {},
   erase: false,
   strokeScale: 16,
@@ -610,7 +610,7 @@ export function RoomLayout() {
   const { id } = useParams();
   const [canManageLayers, setCanManageLayers] = useState(false);
   const [roomName, setRoomName] = useState<string | undefined>(undefined);
-  const [color, setColor] = useState("#000000ff");
+  const [color, setColor] = useState("rgb(0, 0, 0)");
   const [canUndo, setCanUndo] = useState(false);
   const [canRedo, setCanRedo] = useState(false);
   const [sidebarOpen, setSidebarOpen] = useState(true);
--- Inksac-Web/src/components/room-tools/color-selector.tsx
+++ Inksac-Web/src/components/room-tools/color-selector.tsx
@@ -1,8 +1,22 @@
-import { Box, ColorPicker } from "@mantine/core";
+import { Box, ColorInput, ColorPicker, NumberInput } from "@mantine/core";
 import { useRoomLayout } from "../layout/room-layout";
+import { useEffect, useRef, useState } from "react";
 
 export function ColorSelector() {
   const { color, setColor } = useRoomLayout();
+  const [r,g,b,a = 1] = color.match(/[\d.]+/g)!.map(Number);
+  const [localHex, setLocalHex] = useState(
+    `#${r.toString(16).padStart(2,'0')}${g.toString(16).padStart(2,'0')}${b.toString(16).padStart(2,'0')}`.toUpperCase()
+  );
+  const hexInputFocus = useRef(false);
+
+  useEffect(() => {
+    if (!hexInputFocus.current) {
+      setLocalHex(
+        `#${r.toString(16).padStart(2,'0')}${g.toString(16).padStart(2,'0')}${b.toString(16).padStart(2,'0')}`.toUpperCase()
+      )
+    }
+  }, [r, g, b]);
 
   return (
     <>
@@ -39,31 +53,103 @@ export function ColorSelector() {
         .responsive-color-picker .mantine-ColorPicker-sliderOverlay {
           border-radius: 0;
         }
+
+        .sliders .mantine-ColorPicker-sliderOverlay {
+          border-radius: 0;
+        }
+
+        .preview {
+
+        }
       `}</style>
 
-      <Box
-        className="responsive-color-picker"
-        style={{
-          width: "100%",
-          height: "100%",
-          minHeight: 0,
-          overflow: "hidden",
-        }}
-      >
+      <Box>
         <ColorPicker
           fullWidth
-          format="hexa"
+          format="rgba"
           value={color}
           onChange={setColor}
+          mb={6}
+          swatchesPerRow={7}
+          pos={"relative"}
+          classNames={{
+            sliders: "sliders",
+            preview: "preview",
+            body: "body",
+          }}
           styles={{
-            saturationOverlay: {
-              borderRadius: 0,
-            },
-            preview: {
-              "--mantine-radius-sm": "0px",
-            },
+            saturation: { height: 150 },
+            saturationOverlay: { borderRadius: 0 },
+            preview: { "--mantine-radius-sm": "0px" },
+            body: {paddingRight: 71},
           }}
         />
+        <Box 
+          style={{
+            position: "absolute",
+            top: 184,
+            right: 8
+          }}
+        >
+          <ColorInput
+            format="hex"
+            value={localHex}
+            onFocus={() => { hexInputFocus.current = true;}}
+            onBlur={() => {
+              hexInputFocus.current = false;
+              setLocalHex(
+                `#${r.toString(16).padStart(2,'0')}${g.toString(16).padStart(2,'0')}${b.toString(16).padStart(2,'0')}`.toUpperCase()
+              );
+            }}
+            radius={0}
+            size="18.3"
+            withPicker={false}
+            leftSectionWidth={6}
+            styles={{
+              section: { display: "none" },
+              input: {
+                fontSize: "80%"
+              }
+            }}
+            style={{
+              width: 65,
+              marginBottom: 6,
+            }}
+            onChange={(val) => {
+              setLocalHex(val);
+              const hex = val.replace('#', '');
+              if (hex.length !== 6) return;
+
+              const newR = parseInt(hex.slice(0,2), 16);
+              const newG = parseInt(hex.slice(2,4), 16);
+              const newB = parseInt(hex.slice(4,6), 16);
+              setColor(`rgba(${newR}, ${newG}, ${newB}, ${a})`);
+            }}
+          />
+          <NumberInput
+            suffix="%"
+            min={0}
+            max={100}
+            clampBehavior="strict"
+            defaultValue={100}
+            value={Math.round(a * 100)}
+            radius={0}
+            size="18.3"
+            className="mantine-focus-never"
+            styles={{
+              input: { 
+                fontSize: "80%"
+              }
+            }}
+            style={{
+              width: 65
+            }}
+            onChange={(val) => {
+              const newAlpha = (Number(val) / 100).toFixed(2);
+              setColor(`rgba(${r}, ${g}, ${b}, ${newAlpha})`);
+            }}
+          />
+        </Box>
       </Box>
     </>
   );
